a8017f0474f9d3455e910ca6747cc61bb6b49cd1
tueanhtruong <67304687+tueanhtruong@users.noreply.github.com>
2020-10-26 19:31:25 +0700

    login,welcome,content pages

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ app.use(express.static('public'));
 app.set('view engine','pug');
 app.set('views','./public/views');
 
+const userRouter = require('./router/user.router.js');
+const loginRouter = require('./router/login.router.js');
+const adminRouter = require('./router/admin.router.js');
+
 app.get('/', (req, res) => {
     res.render('view1');
 });
 
-app.get('/login',(req,res)=>{
-    res.render('loginview');
-});
 
+
+app.use('/login',loginRouter);
+app.use('/user',userRouter);
+app.use('/admin',adminRouter);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
